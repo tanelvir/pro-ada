dcf895b6c6eae6aa0951bc434c73ee02e1bd5402
Antti J I Kaikkonen <anttkaik@dhp-45.cs.helsinki.fi>
2013-04-18 14:38:36 +0300

    gd

---
+++ OhtuServletM/src/main/java/bibtex/TyyppiArrayList.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bibtex;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anttkaik
+ */
+public class TyyppiArrayList extends ArrayList<String[]> {
+
+    public TyyppiArrayList() {
+        //this = new ArrayList<String[]>();
+    }
+    
+    
+    
+    
+    private static String etsiID(String[] parametrit) {
+        for (String parametri : parametrit) {
+            if (parametri.toLowerCase().startsWith("id@")) {
+                return parametri.substring(3); 
+            }
+        }
+        return null;
+    }
+    
+    //private String 
+    
+    @Override
+    public boolean add(String[] element) {
+        for (String[] parametrit : this) {
+            if (this.etsiID(parametrit).equals(this.etsiID(element))) {
+                return false;
+            }
+        }
+        super.add(element);
+        return true;
+        
+    }
+}
